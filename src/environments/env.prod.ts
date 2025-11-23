bc1bd05545f6fa0488bbd9fbc3ec2224a8a40c41
Frédéric TRAN <frederictran@hotmail.fr>
2025-11-23 01:04:08 +0100

    fix: Subdomain change

--- src/environments/env.prod.ts
+++ src/environments/env.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   frontUrl: 'https://prlive.frederic94500.net',
-  apiUrl: 'https://sorter.prlive.frederic94500.net',
+  apiUrl: 'https://prlive-sorter.frederic94500.net',
 };
