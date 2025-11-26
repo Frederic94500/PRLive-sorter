ac546cc72e226c06424daf3c01ec562f734850dd
Frédéric TRAN <frederictran@hotmail.fr>
2025-11-27 00:36:08 +0100

    feat: Add Index page + RenderMode Client

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,22 @@
 import { Routes } from '@angular/router';
 import { SorterComponent } from './sorter-component/sorter-component';
 import { SorterResolver } from '../resolvers/sorter-resolver';
+import { IndexComponent } from './index-component/index-component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: IndexComponent
+  },
   {
     path: ':prId/:voterId/:sheetId',
     component: SorterComponent,
     resolve: {
       sheet: SorterResolver,
     }
+  },
+  {
+    path: '**',
+    component: IndexComponent
   }
 ];
